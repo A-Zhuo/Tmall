8ca3a4da9cc9bbe87d15f217b412cae113e0276a
A-Zhuo <1208622322@qq.com>
2016-09-22 19:39:51 +0800

    JS

--- scripts/ad.js
+++ scripts/ad.js
@@ -1,14 +1,14 @@
 /*首页广告*/
 $(function() {
-	setInterval("roll()",2500);
+	setInterval("roll()",3000);
 })
 var p = 1;		
 function roll() {
-	if(p < 3) {
+	if(p < 2) {
 		$(".ad").animate({left:'-=795px'},1500);
 		p++;
 	}else{
-		$(".ad").animate({left:"0px"},0);
+		$(".ad").animate({left:"0px"},1500);
 		p = 1;
 	}
 }
