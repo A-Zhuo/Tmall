b7f22866f436d4ba1d0cf6ed532ecdd1b31b91d5
A-Zhuo <1208622322@qq.com>
2016-09-27 14:20:48 +0800

    JS

--- scripts/board.js
+++ scripts/board.js
@@ -4,6 +4,14 @@ window.onload = function() {
 	btn.onclick = function() {
 		var oMsg = document.getElementById("message");
 		var sTxt = oMsg.value;
+		var len = sTxt.length;
+		if(len > 140) {
+			alert("字数不能超过140字！");
+		} else if(len <= 0) {
+			alert("留言内容不能为空！")
+		} else {
+			alert("感谢您的留言！")
+		};
 		var oUl = document.getElementsByClassName("board_msg")[0];
 		var oLi = document.createElement("li");
 		var oNode = document.createTextNode(sTxt);
